d80e612d1dbaba5f15c18cbdab400fc1e9c3f085
Jason Vanstone <jason@chameleoncreative.com>
2022-03-21 16:34:18 -0400

    added style to plugin

--- featured-menu-item.php
+++ featured-menu-item.php
@@ -35,6 +35,16 @@ define( 'FEATURED_MENU_ITEM_VERSION', '1.0.0' );
 
 
 /**
+ * Enqueue Style sheets
+ *
+ * @return mixed
+ */
+function wpdocs_theme_name_scripts() {
+    wp_enqueue_style( 'fmi-stle', et_template_directory_uri() . '/public/css/style.css', array(), '1.00', false );
+    wp_enqueue_script( 'script-name', get_template_directory_uri() . '/public/js/example.js', array(), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
+/**
  * Get the actual date of the week.
  *
  * @return string
@@ -53,30 +63,34 @@ function get_weekday_feature() {
  * @return string
  */
 function fmi_add_to_cart_button( $product ) {
-   
+	?>
+   <div class-="add-quantity-box"> 
+	   <?php
 	if ( $product && $product->is_type( 'simple' ) && $product->is_purchasable() && $product->is_in_stock() && ! $product->is_sold_individually() ) {
-        // Get the necessary classes
-        $class = implode( ' ', array_filter( array(
-            'button',
-            'product_type_' . $product->get_type(),
-            $product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
-            $product->supports( 'ajax_add_to_cart' ) ? 'ajax_add_to_cart' : '',
-        ) ) );
+		// Get the necessary classes
+		$class = implode( ' ', array_filter( array(
+			'button',
+			'product_type_' . $product->get_type(),
+			$product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
+			$product->supports( 'ajax_add_to_cart' ) ? 'ajax_add_to_cart' : '',
+		) ) );
 
-        // Embedding the quantity field to Ajax add to cart button
-        $html = sprintf( '%s<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s">%s</a>',
-            woocommerce_quantity_input( array(), $product, false ),
-            esc_url( $product->add_to_cart_url() ),
-            esc_attr( isset( $quantity ) ? $quantity : 1 ),
-            esc_attr( $product->get_id() ),
-            esc_attr( $product->get_sku() ),
-            esc_attr( isset( $class ) ? $class : 'button' ),
-            esc_html( $product->add_to_cart_text() )
-        );
-    }
-   
-    return $html;
-} 
+		// Embedding the quantity field to Ajax add to cart button
+		$html = sprintf( '%s<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s">%s</a>',
+			woocommerce_quantity_input( array(), $product, false ),
+			esc_url( $product->add_to_cart_url() ),
+			esc_attr( isset( $quantity ) ? $quantity : 1 ),
+			esc_attr( $product->get_id() ),
+			esc_attr( $product->get_sku() ),
+			esc_attr( isset( $class ) ? $class : 'button' ),
+			esc_html( $product->add_to_cart_text() )
+		);
+	}
+	?>
+   </div>
+     <?php
+	return $html;
+}
 
 
 /**
@@ -128,21 +142,9 @@ add_shortcode( 'featured-menu-item', 'fmi_get_featured_menu_item' );
 
 add_action( 'wp_footer' , 'archives_quantity_fields_script' );
 function archives_quantity_fields_script(){
-    ?>
-    <script type='text/javascript'>
-        jQuery(function($){
-            // Update data-quantity
-            $(document.body).on('click input', 'input.qty', function() {
-                $(this).parent().parent().find('a.ajax_add_to_cart').attr('data-quantity', $(this).val());
-                $(".added_to_cart").remove(); // Optional: Removing other previous "view cart" buttons
-            }).on('click', '.add_to_cart_button', function(){
-                var button = $(this);
-                setTimeout(function(){
-                    button.parent().find('.quantity > input.qty').val(1); // reset quantity to 1
-                }, 1000); // After 1 second
-
-            });
-        });
-    </script>
-    <?php
+	?>
+	<script type='text/javascript'>
+	
+	</script>
+	<?php
 }
