2baf8d5b50ad64371384beba6140436e70c6d146
Jason Vanstone <jason@chameleoncreative.com>
2022-03-21 14:03:52 -0400

    added header

--- featured-menu-item.php
+++ featured-menu-item.php
@@ -1,3 +1,20 @@
 /**
- * Plugin Name: YOUR PLUGIN NAME
+ * @package           Featured Menu Item
+ * @author            Jason Vanstone
+ * @copyright         2019 JV
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Featured Menu Item
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Jason Vanstone
+ * Author URI:        
+ * Text Domain:       plugin-fmi
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
  */
