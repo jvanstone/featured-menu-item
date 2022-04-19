31f11032231b97afc911936faffc06df6147a637
Jason Vanstone <jason@chameleoncreative.com>
2022-04-19 11:35:41 -0400

    Cleaned code

--- featured-menu-item.php
+++ featured-menu-item.php
@@ -81,121 +81,7 @@ function fmi_add_to_cart_button( $product ) {
 			esc_attr( isset( $class ) ? $class : 'button' ),
 			esc_html( $product->add_to_cart_text() )
 		);
-	}  elseif ( $product && $product->is_type( 'grouped' ) && $product->is_purchasable() && $product->is_in_stock() && ! $product->is_sold_individually() ) {
-
-		echo 'Grouped';
-
-		global $product, $post;
-
-do_action( 'woocommerce_before_add_to_cart_form' ); ?>
-
-<form class="cart grouped_form" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
-	<table cellspacing="0" class="woocommerce-grouped-product-list group_table">
-		<tbody>
-			<?php
-			$quantites_required      = false;
-			$previous_post           = $post;
-			$grouped_product_columns = apply_filters(
-				'woocommerce_grouped_product_columns',
-				array(
-					'quantity',
-					'label',
-					'price',
-				),
-				$product
-			);
-			$show_add_to_cart_button = false;
-
-			do_action( 'woocommerce_grouped_product_list_before', $grouped_product_columns, $quantites_required, $product );
-
-			foreach ( $grouped_products as $grouped_product_child ) {
-				$post_object        = get_post( $grouped_product_child->get_id() );
-				$quantites_required = $quantites_required || ( $grouped_product_child->is_purchasable() && ! $grouped_product_child->has_options() );
-				$post               = $post_object; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-				setup_postdata( $post );
-
-				if ( $grouped_product_child->is_in_stock() ) {
-					$show_add_to_cart_button = true;
-				}
-
-				echo '<tr id="product-' . esc_attr( $grouped_product_child->get_id() ) . '" class="woocommerce-grouped-product-list-item ' . esc_attr( implode( ' ', wc_get_product_class( '', $grouped_product_child ) ) ) . '">';
-
-				// Output columns for each product.
-				foreach ( $grouped_product_columns as $column_id ) {
-					do_action( 'woocommerce_grouped_product_list_before_' . $column_id, $grouped_product_child );
-
-					switch ( $column_id ) {
-						case 'quantity':
-							ob_start();
-
-							if ( ! $grouped_product_child->is_purchasable() || $grouped_product_child->has_options() || ! $grouped_product_child->is_in_stock() ) {
-								woocommerce_template_loop_add_to_cart();
-							} elseif ( $grouped_product_child->is_sold_individually() ) {
-								echo '<input type="checkbox" name="' . esc_attr( 'quantity[' . $grouped_product_child->get_id() . ']' ) . '" value="1" class="wc-grouped-product-add-to-cart-checkbox" />';
-							} else {
-								do_action( 'woocommerce_before_add_to_cart_quantity' );
-
-								woocommerce_quantity_input(
-									array(
-										'input_name'  => 'quantity[' . $grouped_product_child->get_id() . ']',
-										'input_value' => isset( $_POST['quantity'][ $grouped_product_child->get_id() ] ) ? wc_stock_amount( wc_clean( wp_unslash( $_POST['quantity'][ $grouped_product_child->get_id() ] ) ) ) : '', // phpcs:ignore WordPress.Security.NonceVerification.Missing
-										'min_value'   => apply_filters( 'woocommerce_quantity_input_min', 0, $grouped_product_child ),
-										'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $grouped_product_child->get_max_purchase_quantity(), $grouped_product_child ),
-										'placeholder' => '0',
-									)
-								);
-
-								do_action( 'woocommerce_after_add_to_cart_quantity' );
-							}
-
-							$value = ob_get_clean();
-							break;
-						case 'label':
-							$value  = '<label for="product-' . esc_attr( $grouped_product_child->get_id() ) . '">';
-							$value .= $grouped_product_child->is_visible() ? '<a href="' . esc_url( apply_filters( 'woocommerce_grouped_product_list_link', $grouped_product_child->get_permalink(), $grouped_product_child->get_id() ) ) . '">' . $grouped_product_child->get_name() . '</a>' : $grouped_product_child->get_name();
-							$value .= '</label>';
-							break;
-						case 'price':
-							$value = $grouped_product_child->get_price_html() . wc_get_stock_html( $grouped_product_child );
-							break;
-						default:
-							$value = '';
-							break;
-					}
-
-					echo '<td class="woocommerce-grouped-product-list-item__' . esc_attr( $column_id ) . '">' . apply_filters( 'woocommerce_grouped_product_list_column_' . $column_id, $value, $grouped_product_child ) . '</td>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-
-					do_action( 'woocommerce_grouped_product_list_after_' . $column_id, $grouped_product_child );
-				}
-
-				echo '</tr>';
-			}
-			$post = $previous_post; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-			setup_postdata( $post );
-
-			do_action( 'woocommerce_grouped_product_list_after', $grouped_product_columns, $quantites_required, $product );
-			?>
-		</tbody>
-	</table>
-
-	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" />
-
-	<?php if ( $quantites_required && $show_add_to_cart_button ) : ?>
-
-		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
-
-		<button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
-
-		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
-
-	<?php endif; ?>
-</form>
-
-<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
-	
-<?php
 	}
-
 	return $html;
 }
 
@@ -267,30 +153,13 @@ add_shortcode( 'featured-menu-item', 'fmi_make_feature', 99 );
 
 
 
-
-function woocommerce_grouped_add_to_cart() {
-	global $product;
-
-	$products = array_filter( array_map( 'wc_get_product', $product->get_children() ) );
-
-	if ( $products ) {
-		 usort( $products, 'wc_products_array_orderby_menu_order' );
-
-	wc_get_template( 'single-product/add-to-cart/grouped.php', array(
-		'grouped_product'    => $product,
-		'grouped_products'   => $products,
-		'quantites_required' => false,
-	) );
- }
-}
-
 /**
  * Display Featured Menu Item by day.
  *
  * @return void
  */
 function fmi_get_featured_menu_daily( $data ) {
-	global $product; 
+	global $product;
 
 	$data = shortcode_atts(
 		array(
@@ -311,10 +180,6 @@ function fmi_get_featured_menu_daily( $data ) {
 	);
 	$loop          = new WP_Query( $args );
 	$product_count = $loop->post_count;
-
-	
-
-
 	?>
 
 	<div class="fmi-daily-container">
@@ -323,57 +188,46 @@ function fmi_get_featured_menu_daily( $data ) {
 
 		if ( $product_count > 0 ) {
 			$product = wc_get_product( $loop->post->ID );
-		
+			?>
+			<div class="half-side">
+				<div id="product-image1">
+						<a href="<?php echo esc_url( get_permalink( $product->id ) ); ?>" title="<?php echo esc_attr( $product->get_title() ); ?>">
+						<?php echo $product->get_image( 'full' );?>
+						</a>
+				</div> <!-- End Product Image -->
+			</div>
 
-		
-		?>
-		<div class="half-side">
-		<div id="product-image1">
+			<div class="half-side">
 				<a href="<?php echo esc_url( get_permalink( $product->id ) ); ?>" title="<?php echo esc_attr( $product->get_title() ); ?>">
-				<?php echo $product->get_image('full');?>
-				</a>
-		</div> <!-- End Product Image -->
+				<h4><?php echo $product->get_title(); ?></h4></a>
 
-		</div>
+				<h6><?php //echo $product->get_price_html(); ?></h6>
+				<p><?php echo $product->get_short_description(); ?></p>
 
+				<div class="add-quantity-box"> 
+				<?php
+				if ( $product->is_type( 'grouped' ) ) {
 
+					ob_start();
+						woocommerce_grouped_add_to_cart();
+						woocommerce_template_loop_add_to_cart();
+					$output = ob_get_clean();
+					echo $output;
 
-		<div class="half-side">
-			<a href="<?php echo esc_url( get_permalink( $product->id ) ); ?>" title="<?php echo esc_attr( $product->get_title() ); ?>">
-			<h4><?php echo $product->get_title(); ?></h4></a>
-
-			
-			<h6><?php //echo $product->get_price_html(); ?></h6>
-			<p><?php echo $product->get_short_description(); ?></p>
-
-			<div class="add-quantity-box"> 
-			<?php
-			if( $product->is_type( 'grouped' ) ) {
-				$products = $product->get_children();
+				} else {
+					echo fmi_add_to_cart_button( $product );
 
 				?>
-				<p>View the product and select your products</p>
-				<?php
-				ob_start();
-				woocommerce_template_loop_add_to_cart();
-				$output = ob_get_clean();
-				echo '<div class="quantity">' . $output . '</div>';
-
-			} else {  
-				echo fmi_add_to_cart_button( $product ); 
-
-				
-			?>
-			<?php } ?>
+				<?php } ?>
+				</div>
 			</div>
-		</div>
-	
-		<?php
 
-	} else {
-		echo 'No product matching your criteria.';
-	}
-	?>
+				<?php
+
+		} else {
+			echo esc_html( 'No product matching your criteria.' );
+		}
+		?>
 	</div> 
 	<?php
 
